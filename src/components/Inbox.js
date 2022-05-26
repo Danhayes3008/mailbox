2c11e7254a6e92caabd0724cdd17cafae6eefa13
Danhayes3008 <43347669+Danhayes3008@users.noreply.github.com>
2022-05-26 14:17:22 +0100

    created a table to display the array

--- src/components/Inbox.js
+++ src/components/Inbox.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const unreadMessages = [
-  {title: 'React'},
-  {title: 'Re:React'},
-  {title: 'Re:Re:React'}
+  {sender: 'lizzy@gmail.com', title: 'React', date: '22/05/2022'},
+  {sender: 'lizzy@gmail.com', title: 'Re:React', date: '23/05/2022'},
+  {sender: 'lizzy@gmail.com', title: 'Re:Re:React', date: '23/05/2022'}
 ]
 
 function Inbox() {
@@ -23,9 +23,22 @@ function Inbox() {
 export const Messages = () => {
   return (
     <div className='messages'>
-      {unreadMessages.map(({title}) => (
-        <p key={title}>{title}</p>
-      ))}
+      <table>
+        <thead>
+          <tr>
+            <th>Sender</th>
+            <th>title</th>
+            <th>date</th>
+          </tr>
+        </thead>
+          {unreadMessages.map(({sender, title, date}) => (
+            <><tr>
+              <td key={sender}><input type="checkbox" name="check" /> {sender}</td>
+              <td key={title}>{title}</td>
+              <td key={date}>{date}</td>
+            </tr></>
+          ))}
+      </table>
     </div>
   )
 }
